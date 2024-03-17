d3d18cf0057b57c5c69311c25b478848472e9361
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-03-17 14:53:24 +0330

    create section Accordion

---
+++ src/components/module/Accordion/Accordion.js
@@ -0,0 +1,28 @@
+"use client"
+import React, { useState } from 'react';
+import style from './Accordion.module.css';
+import { FaPlus } from "react-icons/fa";
+
+function Accordion() {
+    const [showAccordion , setShowAccordion] = useState(false);
+
+    const handlerShowAccordion = ()=>{
+        setShowAccordion(!showAccordion)
+    }
+
+  return (
+    <div className={style.accordion}>
+    <div className={style.accordion_title_icon} onClick={handlerShowAccordion}>
+        <span className={style.accordion_icon}>
+            <FaPlus/>
+        </span>
+        <h6 className={style.accordion_title}> چرا ما از این شرکت محصولاتی را خریداری می کنیم؟ </h6>
+    </div>
+    {showAccordion && (
+    <p className={showAccordion ? style.accordion_desc_ani : style.accordion_desc}> لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد صنعت بوده است. </p>
+    )}
+</div>
+  )
+}
+
+export default Accordion
