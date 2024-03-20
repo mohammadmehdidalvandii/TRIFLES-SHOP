711412936f4d387d5ead13da8a7043a9a24624b5
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-03-20 14:56:48 +0330

    create page services

---
+++ src/app/Services/page.js
@@ -0,0 +1,14 @@
+import PageHeader from '@/components/module/PageHeader/PageHeader'
+import React from 'react'
+
+function page() {
+  return (
+    <>
+            <PageHeader
+                title='خدمات'
+            />
+     </>
+  )
+}
+
+export default page
