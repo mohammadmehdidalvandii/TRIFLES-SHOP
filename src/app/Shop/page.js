723d4a559f7a66c0c5ba16057c474b945c2e833d
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-03-20 14:17:37 +0330

    create page Shop

---
+++ src/app/Shop/page.js
@@ -0,0 +1,14 @@
+import PageHeader from '@/components/module/PageHeader/PageHeader'
+import React from 'react'
+
+function Shop() {
+  return (
+   <>
+    <PageHeader
+      title='فروشگاه'
+    />
+   </>
+  )
+}
+
+export default Shop
