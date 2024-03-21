d5f8a7cccb0c6f76d6a4aca56544abf1e67cb784
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-03-21 15:06:36 +0330

    creare page productdetails

---
+++ src/app/Shop/[id]/page.js
@@ -0,0 +1,14 @@
+import PageHeader from '@/components/module/PageHeader/PageHeader';
+import React from 'react';;
+
+function ProductDetails() {
+  return (
+   <>
+    <PageHeader
+      title='جزئیات محصول'
+    />
+   </>
+  )
+}
+
+export default ProductDetails
