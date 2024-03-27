22a5e77392acc03a33a71f73dd58a3d76f37a3ee
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-03-27 15:44:14 +0330

    create file Auth

---
+++ src/utils/Auth.js
@@ -0,0 +1,62 @@
+import connectToDB from "@/configs/db";
+import { hash , compare } from "bcryptjs";
+import { sign , verify } from "jsonwebtoken";
+import { cookies } from "next/headers";
+import UserModel from '@/models/User'
+
+
+const hashPassword = async (password)=>{
+    const hashedPassword = await hash(password, 12)
+    return hashedPassword;
+}
+
+const verifyPassword = async (password , hashPassword)=>{
+    const isValid = await compare(password , hashPassword);
+    return isValid
+}
+
+const generateAccessToken = (data) => {
+    const token = sign({...data}, process.env.ACCESS_TOKEN_SCT, {
+        expiresIn: "60d",
+    });
+    return token;
+}
+
+const verifyAccessToken = (token)=>{
+    try{
+        const tokenPayload = verify(token , process.env.ACCESS_TOKEN_SCT)
+        return tokenPayload
+    }catch(err){
+        console.log("verify Access Token Error -->" , err)
+        return false
+    }
+}
+
+const generateRefreshToken = (data)=>{
+    const token = sign({...data}, process.env.REFRESH_TOKEN_SCT, {
+        expiresIn: "15d"
+    });
+    return token
+}
+
+
+const authUser = async ()=>{
+    connectToDB()
+    const token =   cookies().get("token");
+    let user = null
+    if(token){
+        const tokenPayload = verifyAccessToken(token.value);
+        if(tokenPayload){
+            user = await UserModel.findOne({email : tokenPayload.email})
+        }
+    }
+    return user
+}
+
+export{
+    hashPassword,
+    verifyPassword,
+    generateAccessToken,
+    verifyAccessToken,
+    generateRefreshToken
+}
