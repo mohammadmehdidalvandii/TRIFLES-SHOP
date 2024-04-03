95cea4f0d2dfc202f8853fca46eb9a2eabfd8033
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-04-03 14:35:00 +0330

    fix auth user

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -40,18 +40,7 @@ const generateRefreshToken = (data)=>{
 }
 
 
-const authUser = async ()=>{
-    connectToDB()
-    const token =   cookies().get("token");
-    let user = null
-    if(token){
-        const tokenPayload = verifyAccessToken(token.value);
-        if(tokenPayload){
-            user = await UserModel.findOne({email : tokenPayload.email})
-        }
-    }
-    return user
-}
+
 
 export{
     hashPassword,
@@ -59,5 +48,4 @@ export{
     generateAccessToken,
     verifyAccessToken,
     generateRefreshToken,
-    authUser,
 }
